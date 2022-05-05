d6dec26d2b5c96de5fb8df0d21d238531cee5649
Tiago Dinis <tiagodinis33@gmail.com>
2022-05-05 23:10:22 +0100

    Ready. Set. GOOOO!

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='center'>
+        <div id="i-am">Oi, eu sou</div>
+        <h1 id="name">Tiago Dinis</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
