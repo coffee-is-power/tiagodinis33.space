41ac32f6eec596616fa46fa41b61609fb4fddc9c
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-05-06 15:58:50 +0000

    Add "know more" button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <div className='center'>
         <div id="i-am">Oi, eu sou</div>
         <h1 id="name">Tiago Dinis</h1>
+        <button id="know-more">Saber Mais</button>
       </div>
     </div>
   )
