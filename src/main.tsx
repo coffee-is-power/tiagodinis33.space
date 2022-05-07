85bf0005a802c92f37920e62882e7861926de100
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-05-07 15:26:50 +0000

    Use url hash instead of path

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-let Component
-switch (window.location.pathname) {
-  case "/about-me":
-    Component = import('./AboutMe')
-    break;
-  default:
-    Component = import('./App');
-    break; 
-}
-(async () => {
-  let Comp = (await Component).default;
-  ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+async function render() {
+  let Component
+  switch (window.location.hash) {
+    case "#/about-me":
+      Component = (await import('./AboutMe')).default
+      break;
+    default:
+      Component = (await import('./App')).default;
+      break; 
+  }
+  root.render(
     <React.StrictMode>
-      <Comp />
+      <Component />
     </React.StrictMode>
   )
-})();
+}
+render();
+window.addEventListener('hashchange', render)
