90363de88221404592a150f9da5c839976be1cb0
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-05-08 00:24:05 +0000

    Cleaned up navbar

--- src/main.tsx
+++ src/main.tsx
@@ -19,17 +19,10 @@ async function render() {
   root.render(
     <React.StrictMode>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#/">Home</Nav.Link>
-              <Nav.Link href="#/about-me">Sobre Mim</Nav.Link>
-              
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Nav className="me-auto m-auto">
+          <Nav.Link href="#/">Home</Nav.Link>
+          <Nav.Link href="#/about-me">Sobre Mim</Nav.Link>
+        </Nav>
       </Navbar>
       <Component />
     </React.StrictMode>
