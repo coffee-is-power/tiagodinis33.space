278784d3e2ea73cc5ea0321ad9dd467de1e8c34a
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-05-08 00:04:57 +0000

    Added about me page

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,3 +1,43 @@
+import "./AboutMe.css";
 export default function AboutMe(){
-    return <>Hello World!</>
+    
+    return <>
+        <section className="flex">
+            <div>
+                <img height="200" src="/src/nlogin-js.png"/>
+            </div>
+            <div>
+                <h1>nLogin JS</h1>
+                <p>Permite integrar o nlogin usando java script de maneira muito mais facil</p>
+                <p>Source: <a href="https://gitlab.com/tiagodinis33/nlogin-js">Gitlab</a></p>
+            </div>
+        </section>
+
+        <section className="flex">
+            <div>
+                <h1>TasteBin</h1>
+                <p>É só uma copia do paste bin... foi a primeira vez que usei cypress :)</p>
+                <p>Source: <a href="https://github.com/tiagodinis33/tastebin">GitHub</a></p>
+                <p>Site: <a href="https://tastebin.vercel.app">tastebin.vercel.app</a></p>
+            </div>
+            <div>
+                <img height="300" src="/src/tastebin.gif"/>
+            </div>
+        </section>
+        <section className="flex">
+            <div>
+                <h1>Yet Another NES Emulator (YANE)</h1>
+                <p>É um emulador de NES feito em rust que estou programando por curiosidade</p>
+                <p>Está em <u>desenvolvimento</u>, não é usável <u>ainda</u></p>
+                <p>Source: <a href="https://github.com/tiagodinis33/yane">GitHub</a></p>
+            </div>
+        </section>
+        <footer>
+            <h3>Contatos:</h3>
+            <p>Email: <a href="mailto:tiagodinis33@gmail.com">tiagodinis33@gmail.com</a></p>
+            <p>Discord: Trustacean#7722</p>
+            
+        </footer>
+
+    </>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 async function render() {
   let Component
@@ -15,6 +18,19 @@ async function render() {
   }
   root.render(
     <React.StrictMode>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#/">Home</Nav.Link>
+              <Nav.Link href="#/about-me">Sobre Mim</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Component />
     </React.StrictMode>
   )
